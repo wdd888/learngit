b15d21679ce6d8e88625fe08b1433f191f3adb27
wdd888 <wudangdang88@163.com>
2019-08-20 16:09:56 +0800

    测试下

---
+++ src/main/java/com/bmsoft/Swagger2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.bmsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2DemoApplication.class, args);
+	}
+
+}
